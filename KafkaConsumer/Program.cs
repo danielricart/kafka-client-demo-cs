6a1c0e7e99d69b25f7d3a028e94ad49996d71850
Daniel Ricart <daniel.ricart@scmspain.com>
2016-01-25 17:03:44 +0100

    Initial commit

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KafkaNet;
+using KafkaNet.Common;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace ApacheKafkaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consume();
+        }
+
+        private static void Consume()
+        {
+            var broker = "http://localhost:9092";
+            var topic = "MyTopic";
+
+            var options = new KafkaOptions(new Uri(broker));
+            var router = new BrokerRouter(options);
+            var consumer = new Consumer(new ConsumerOptions(topic, router));
+
+            //Consume returns a blocking IEnumerable (ie: never ending stream)
+            foreach (var message in consumer.Consume())
+            {
+                Console.WriteLine("Response: Partition {0},Offset {1} : {2}",
+                    message.Meta.PartitionId, message.Meta.Offset, message.Value.ToUtf8String());
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KafkaNet;
+using KafkaNet.Common;
+using KafkaNet.Model;
+using KafkaNet.Protocol;
+
+namespace ApacheKafkaDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Produce();
+                System.Threading.Thread.Sleep(3000);
+            } while (true);
+        }
+
+        private static void Produce()
+        {
+            var broker = "http://localhost:9092";
+            var topic = "MyTopic";
+
+            var options = new KafkaOptions(new Uri(broker));
+            var router = new BrokerRouter(options);
+            var client = new Producer(router);
+
+            var current_datetime =DateTime.Now;
+            var key = current_datetime.Second.ToString();
+            var events = new[] { new Message("Hello World " + current_datetime.ToString(), key) };
+            client.SendMessageAsync(topic, events).Wait(1500);
+            Console.WriteLine("Produced: Key: {0}. Message: {1}", key, events[0].Value.ToUtf8String());
+
+            using (client) { }
+        }
+    }
+}
